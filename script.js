5716c5a02e1e1c3d95bfd9906e5badc7d9363fda
firshandy <firshandydwi@gmail.com>
2023-09-05 09:33:09 +0700

    tambahan service section

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-// fixed blur navbar
-window.onscroll = function () {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
-    const toTop = document.querySelector('#to-top')
 
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed');
-        toTop.classList.remove('hidden');
-        toTop.classList.add('flex');
-    } else {
-        header.classList.remove('navbar-fixed');
-        toTop.classList.remove('flex');
-        toTop.classList.add('hidden');
-    }
-}
 
 // hamburger menu
 const menuToggle = document.querySelector('.menu-toggle input');
